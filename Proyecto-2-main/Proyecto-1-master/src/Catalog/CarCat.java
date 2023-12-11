29411028fcd1bb52de8b909932c8ec38de5acaf6
jzarruke <123590839+jzarruke@users.noreply.github.com>
2023-12-10 21:05:27 -0500

    Separación archivo Car

---
+++ Proyecto-2-main/Proyecto-1-master/src/Catalog/CarCat.java
@@ -0,0 +1,70 @@
+package Catalog;
+
+import java.util.ArrayList;
+
+import Catalog.Carro;
+
+public class CarCat {
+	
+	private ArrayList<Carro> carros;
+	
+
+	public CarCat() {
+		carros = new ArrayList<Carro>();
+			
+	}
+
+	public void agregarCarro(Carro carro) {
+		carros.add(carro);
+	
+	}
+
+	public Carro buscarCarro(Integer ID) {
+		for (Carro carro : carros) {
+			if (carro.getid().equals(ID) ) {
+				return carro;
+			}
+		}
+		return null;
+	}
+	
+	public void eliminarCarro(Integer ID) {
+		Carro carro = buscarCarro(ID);
+		if (carro != null) {
+			carros.remove(carro);
+		}
+	}
+
+	public ArrayList<Carro> listarCarros() {
+		return carros;
+	}
+
+	public void resumenCatalogo() {
+		System.out.println("Catálogo de carros:");
+		for (Carro carro : carros) {
+			System.out.println(carro);
+		}
+	
+	}
+
+	public void cambiarLugar(Integer ID, String newPOS) {
+		Carro carro = buscarCarro(ID);
+		if (carro != null) {
+			carro.setPOS(newPOS);
+		}
+	}
+
+	public void cambiarAvailable(Integer ID, boolean newAvailable) {
+		Carro carro = buscarCarro(ID);
+		if (carro != null) {
+			carro.setAvailable(newAvailable);
+		}
+	}
+
+	public void cambiarEstado(Integer ID, String newState) {
+		Carro carro = buscarCarro(ID);
+		if (carro != null) {
+			carro.setState(newState);
+		}
+	}
+}
